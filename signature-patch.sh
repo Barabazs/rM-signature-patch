f23c58e244fabc006e14dbcc46b03a051e8054c5
Barabas <31799121+Barabazs@users.noreply.github.com>
2021-03-14 07:28:02 +0100

    Update signature-patch.sh
    Add check for opkg

--- signature-patch.sh
+++ signature-patch.sh
@@ -1,11 +1,19 @@
 #!/opt/bin/sh
 set -e
 
+if ! command -v opkg &>/dev/null; then
+    echo "opkg could not be found"
+    echo "Please install it first. https://github.com/toltec-dev/toltec"
+    exit 1
+fi
+
 if ! command -v perl &>/dev/null; then
     echo "perl could not be found"
     echo "Downloading and installing perl"
     opkg install perl
 fi
+
+echo "Attempting to patch signature"
 perl -pi -e "s/Sent from my/\0ent from my/" -s "/usr/bin/xochitl"
 
 systemctl restart xochitl
