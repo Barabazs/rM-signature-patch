58af2b4a05bc0bc5d7a5824186f57b81a48f45af
John Simpson <jms1@jms1.net>
2023-06-25 20:52:11 -0400

    Only look for `opkg` if needed to install `perl`

--- signature-patch.sh
+++ signature-patch.sh
@@ -1,14 +1,13 @@
 #!/opt/bin/sh
 set -e
 
-if ! command -v opkg &>/dev/null; then
-    echo "opkg could not be found"
-    echo "Please install it first. https://github.com/toltec-dev/toltec"
-    exit 1
-fi
-
 if ! command -v perl &>/dev/null; then
     echo "perl could not be found"
+    if ! command -v opkg &>/dev/null; then
+        echo "opkg could not be found"
+        echo "Please install it first. https://github.com/toltec-dev/toltec"
+        exit 1
+    fi
     echo "Downloading and installing perl"
     opkg install perl
 fi
